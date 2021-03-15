4df4a2dc112a602e733a9070807169129cf6532f
Etienne Schmitz <contact@etienne-schmitz.com>
2021-03-15 08:46:48 +0100

    refacto - Remove opponents publish topics for each robot
    This information is not necessary if we have only informations for opponents.

--- rostron_filters_py/utils/StatePublisher.py
+++ rostron_filters_py/utils/StatePublisher.py
@@ -4,14 +4,14 @@ class StatePublisher():
 
     def __init__(self, node):
         self.node_ = node
-        self.pub_allies_ = self.node_.create_publisher(RobotsMsg, 'allies', 10)
-        self.pub_opponents_ = self.node_.create_publisher(RobotsMsg, 'opponents', 10)
+
         self.pub_ball_ = self.node_.create_publisher(BallMsg, 'ball', 10)
 
+        self.pub_allies_ = self.node_.create_publisher(RobotsMsg, 'allies', 10)
         self.pub_allies_arr_ = [self.node_.create_publisher(
             RobotMsg, 'allies/r_%d' % c, 10) for c in range(16)]
-        self.pub_opponents_arr_ = [self.node_.create_publisher(
-            RobotMsg, 'opponents/r_%d' % c, 10) for c in range(16)]
+        
+        self.pub_opponents_ = self.node_.create_publisher(RobotsMsg, 'opponents', 10)
 
     def publish_ball(self, ball):
         self.pub_ball_.publish(ball)
@@ -22,6 +22,4 @@ class StatePublisher():
         self.pub_allies_.publish(allies)
 
     def publish_opponents(self, opponents):
-        for id, r in enumerate(opponents.robots):
-            self.pub_opponents_arr_[id].publish(r)
         self.pub_opponents_.publish(opponents)
